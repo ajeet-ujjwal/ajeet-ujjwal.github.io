bc9486078eee255c03d649ef73dfb1b7122c2dc6
ajeet.ujjwal <ajeet.ujjwal@PCN-808.local>
2022-03-21 21:38:30 +0900

    Add Usergram to website.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function _sendConversionToUsergram() {
+    usergram.push(['send','Ugo97x-1','cv','click_send_button_cv']);
+}
+
+function _sendAboutMeEventToUsergram() {
+    usergram.push(['send','Ugo97x-1','event','click_about_me_button']);
+}
+
+function _sendContactEventToUsergram() {
+    usergram.push(['send','Ugo97x-1','event','click_contact_button']);
+}
